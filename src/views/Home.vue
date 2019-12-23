7ed9a497498e8482918afe27134a32cedd128df9
srad1292 <smr1292@yahoo.com>
2019-12-22 23:24:16 -0500

    1.0.0 date selection header (#1)
    Date Picker Setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,79 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <div>"Welcome to Your Vue.js App"</div>
+    <div class="center date-container">
+      <h1>Daily Planner</h1>
+      <form>
+        <button class="arrow-button" v-on:click.stop.prevent="updateDate('last')" :disabled="loading">&lt;</button>
+        <input name="view-date" id="date-input" type="date" v-model="pickerDate" v-on:input="loadScheduleAndTasks(pickerDate)" :disabled="loading" required> 
+        <button class="arrow-button" v-on:click.stop.prevent="updateDate('next')" :disabled="loading">&gt;</button>
+      </form>
+    </div>
+
+    <template v-if="loading">
+      <div class="center">
+        Loading for: {{pickerDate}}
+      </div>
+    </template>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 
-export default {
-  name: "home",
-};
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import moment from "moment";
+
+@Component
+export default class Home extends Vue {
+  pickerDate: string;
+  loading: boolean = false;
+
+  constructor() {
+    super();
+    this.pickerDate = "2019-12-22";
+  }
+
+  updateDate(direction: string) {
+    let newDate;
+    if(direction === "last") {
+      newDate = moment(this.pickerDate).subtract(1, 'days');
+    }
+    else {
+      newDate = moment(this.pickerDate).add(1, 'days');
+    }
+    this.pickerDate = newDate.format("YYYY-MM-DD");
+    this.loadScheduleAndTasks(newDate.format("YYYY-MM-DD"));
+  }
+
+  loadScheduleAndTasks(date: string) {
+    this.loading = true;
+    let delay = (time: number) => (result: any) => new Promise(resolve => setTimeout(() => resolve(result), time));
+
+    Promise.resolve("Data")
+      .then(delay(1500))
+      .then(result => this.loading = false);
+    }
+}
 </script>
+
+<style scoped>
+
+.date-container {
+  display: block;
+}
+
+.center {
+  text-align: center;
+}
+
+.arrow-button {
+  margin-left: 3px;
+  margin-right: 3px;
+  font-size: large;
+}
+
+#date-input {
+  font-weight: bold;
+  font-size: large;
+}
+
+</style>
