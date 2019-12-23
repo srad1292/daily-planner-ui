48eb1dadf754e2565fe82d2074a18767152a0662
Sam Radford <smr1292@yahoo.com>
2019-12-22 21:23:01 -0500

    initial setup

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <div>"Welcome to Your Vue.js App"</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "home",
+};
+</script>
