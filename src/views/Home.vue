ed4516c5775b18cf906e552857beb13aa13a8656
Sam Radford <smr1292@yahoo.com>
2019-12-23 11:17:11 -0500

    Added daily task collapsible with frontend data stubbed in

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,6 +14,34 @@
         Loading for: {{pickerDate}}
       </div>
     </template>
+
+    <b-container id="main-content">
+      <b-row>
+        <b-col cols="3">
+          <b-button v-b-toggle.todays-tasks variant="info" class="m-1 collapse-button" v-on:click.stop.prevent="updateDailyIcon()">Today's Tasks {{dailyIcon}}</b-button>
+        
+          <b-collapse id="todays-tasks" class="collapse-content">
+            <ul>
+              <li v-for="task in dailyTasks" v-bind:key="task.id">{{task}}</li>
+            </ul>
+            <form id="daily-task-form">
+              <b-input-group>
+                <b-form-input name="daily-task-input" id="daily-task-input" type="text" v-model="newDailyTask" :disabled="loading"></b-form-input>
+                <b-input-group-append>
+                  <b-button variant="success" v-on:click.stop.prevent="addDailyTask()" :disabled="newDailyTask.length === 0 || loading">Add Task</b-button>
+                </b-input-group-append>
+              </b-input-group>
+            </form>
+          </b-collapse>
+        </b-col>
+
+        <b-col></b-col>
+
+        <b-col cols="8">
+
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -26,10 +54,34 @@ import moment from "moment";
 export default class Home extends Vue {
   pickerDate: string;
   loading: boolean = false;
+  dailyIcon: string;
+  dailyTasks: string[];
+  newDailyTask: string;
 
   constructor() {
     super();
-    this.pickerDate = "2019-12-22";
+    this.pickerDate = moment().format("YYYY-MM-DD");
+    this.dailyIcon = "+";
+    this.dailyTasks = ["Daily Task Feature", "To Do List", "Get Taco Bell"];
+    this.newDailyTask = "";
+  }
+
+  updateDailyIcon() {
+    this.dailyIcon = this.dailyIcon === "+" ? "-" : "+";
+  }
+
+  addDailyTask() {
+    if (this.newDailyTask.length > 0) {
+      this.loading = true;
+      let delay = (time: number) => (result: any) => new Promise(resolve => setTimeout(() => resolve(result), time));
+      Promise.resolve("Data")
+        .then(delay(1500))
+        .then(result => { 
+          this.dailyTasks.push(this.newDailyTask);
+          this.newDailyTask = "";
+          this.loading = false
+        });
+    }
   }
 
   updateDate(direction: string) {
@@ -51,11 +103,15 @@ export default class Home extends Vue {
     Promise.resolve("Data")
       .then(delay(1500))
       .then(result => this.loading = false);
-    }
+  }
 }
 </script>
 
 <style scoped>
+
+ul {
+  text-align: left;
+}
 
 .date-container {
   display: block;
@@ -71,9 +127,36 @@ export default class Home extends Vue {
   font-size: large;
 }
 
+.collapse-button {
+  width: 94%;
+}
+
+.collapse-content {
+  width: 94%;
+  margin-left: 3%;
+  border: 1px solid #17a2b8;;
+}
+
+#home {
+  background-color: #f7f8fc;
+}
+
 #date-input {
   font-weight: bold;
   font-size: large;
+}
+
+#main-content {
+  max-width: 100%;
+  width: 86%;
+  margin-left: 7%;
+  margin-top: 3%;
+}
+
+#daily-task-form {
+  width: 94%;
+  margin-left: 3%;
+  margin-bottom: 2px;
 }
 
 </style>
